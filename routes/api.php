c36309b95a2d3151144e630e8f10758aa7ab7b3e
Zak Nesler <zaknesler@gmail.com>
2020-05-19 10:05:33 -0400

    Remove demo tool

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,3 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::group(['middleware' => 'api'], function () {
-    Route::post('/tools/demo', 'Tools\DemoController')->name('api.tools.demo');
-});
+//
--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,6 @@ use Illuminate\Support\Facades\Route;
  */
 Route::view('/', 'about')->name('about');
 Route::view('/projects', 'projects')->name('projects');
-Route::view('/tools', 'tools')->name('tools');
-
-/*
- * Tools
- */
-Route::view('/tools/demo', 'tools/demo')->name('tools.demo');
 
 /*
  * Resume
