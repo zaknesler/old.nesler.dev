be29bd7e2f67375ca222e0e8a480778cb36f5750
Zak Nesler <zaknesler@gmail.com>
2020-05-20 14:51:11 -0400

    Remove paragraph

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,8 +4,7 @@
   <div class="markup">
     <p><strong>Hey!</strong> I'm Zak, a 20-year-old student full-stack developer from Pennsylvania with a passion for tasteful and minimal design, as well as tidy and efficient back-end code.</p>
     <p>Over the past few years, I have fallen in love with the <a href="https://laravel.com">Laravel</a> PHP framework, the <a href="https://vuejs.org">Vue</a> JavaScript framework, and <a href="https://tailwindcss.com">Tailwind CSS</a>, a utility-first CSS framework, and would consider myself to be very proficient in each.</p>
-    <p>I began programming in middle school and have since become fluent in PHP, HTML, CSS (Less, Sass, etc.), JavaScript, Java, and Python, with an ever-growing desire to learn more.</p>
-    <p>I am currently pursuing a Bachelor of Science degree in Computer Science, hoping to gain more knowledge and experience doing what I love. In the meantime I like to work on side projects and contribute what I can to <a href="https://github.com/zaknesler">open source</a>.</p>
+    <p>I am currently pursuing a Bachelor of Science degree in Computer Science, hoping to gain more knowledge and experience doing what I love. In the meantime I like to work on side projects and contribute what I can to open source.</p>
     <p>If you’re interested in knowing more about me, please feel free to take a look at my <a href="{{ route('resume') }}">résumé</a> or send me an <a href="mailto:zak@nesler.dev">email</a> and I'll get back to you!</p>
   </div>
 @endsection
