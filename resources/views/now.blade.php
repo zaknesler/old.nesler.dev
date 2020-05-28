4fc732621370003605c67f71347c432daf366ef8
Zak Nesler <zaknesler@gmail.com>
2020-05-27 23:23:23 -0400

    Update now page

--- resources/views/now.blade.php
+++ resources/views/now.blade.php
@@ -9,14 +9,14 @@
 
   <div class="mt-6 markup">
     <h4>College</h4>
-    <p>This year I finished my second year of my B.S. degree in Computer Science. My CS classes this year focused on creating data structures using Java and C++, such as trees and graphs.</p>
+    <p>This year I finished my second year of my B.S. degree in Computer Science. My CS classes this year focused on creating data structures using Java and C++, such as trees, graphs, linked lists, queues, and stacks.</p>
 
     <h4>Web Development</h4>
     <p>This year I hope to spend some time learning new technologies such as React, Electron, and GraphQL. I don't think I'd ever switch off of Vue.js, but I would certainly like to learn more!</p>
 
     <h4>Adventures into Art</h4>
-    <p>This year I also began venturing into art, and with it I started learning <a href="https://blender.org">Blender</a>, an incredibly powerful, open-source 3D suite. I have had a blast learning about 3D modeling, physics and fluid simulations, and creating awesome scenes in general using free tutorials on YouTube.</p>
-    <p>As a side hobby, I plan on continuing my adventure into 3D art as well as learning traditional art, with the hope that I can create the things from my imagination to share with others.</p>
+    <p>This year I also began venturing into art, and with it I started learning <a href="https://blender.org">Blender</a>, an incredibly powerful, open-source 3D suite. I have had a blast learning about 3D modeling, physics and fluid simulations, and creating awesome scenes in general, all using free tutorials on YouTube.</p>
+    <p>As a side hobby, I plan on continuing my adventure into 3D art as well as learning traditional art, with the hope that I can create the crazy things from my imagination.</p>
     <p>You can follow me on my <a href="https://artstation.com/zaknesler">ArtStation</a> if you would like, which is where I'll post any of the projects I work on!</p>
   </div>
 @endsection
