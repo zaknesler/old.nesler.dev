698be318a042b22a28f127866a05c860d58cfc8e
Zak Nesler <zaknesler@gmail.com>
2020-08-18 21:43:59 -0400

    Add additional about page test

--- tests/Feature/Pages/AboutPageTest.php
+++ tests/Feature/Pages/AboutPageTest.php
@@ -9,6 +9,15 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class AboutPageTest extends TestCase
 {
     /** @test */
+    function the_index_route_displays_about_page()
+    {
+        $response = $this->get('/');
+
+        $response->assertSuccessful();
+        $response->assertSee('Hey!');
+    }
+
+    /** @test */
     function guest_can_view_about_page()
     {
         $response = $this->get(route('about'));
